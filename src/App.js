a0a4b21346173912511bd7ce4480676cd41eff64
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-15 00:23:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // import logo from './logo.svg';
-import './App.scss';
+import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Components/Layout'
+import Home from './Componets/Home'
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,12 +1,35 @@
+// import './index.scss'
+// import Sidebar from '../Sidebar'
+
+// import React from 'react'
+
+// export default function index() {
+//   return (
+//     <>
+//       <Sidebar />
+//     </>
+//   )
+// }
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar/'
 import './index.scss'
-import Sidebar from '../Sidebar'
 
-import React from 'react'
-
-export default function index() {
+const Layout = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
   )
 }
+
+export default Layout
