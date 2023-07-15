ffdf4c82375ba47e9cb341f34d27439202d8a1b3
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-16 02:04:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './Components/Home'
-import About from './Components/About'
-import Contact from './Components/Contact'
-import Layout from './Components/Layout'
-import './App.scss'
+import React from "react"
+import Header from "./Components/Head/Header"
 
-function App() {
+import "./App.css"
+
+function App () {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
+      <Header />
+     
     </>
   )
 }
