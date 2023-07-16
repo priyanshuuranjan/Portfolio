a46139ec2d5494b9cdc59080be15ff44a970883e
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-16 19:48:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./Components/Head/Header"
 import Home from "./Components/Hero/Home"
+import Features from "./Components/Features/Features"
 
 import "./App.css"
 
@@ -9,6 +10,7 @@ function App () {
     <>
       <Header />
      <Home />
+     <Features />
     </>
   )
 }
