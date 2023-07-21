fa6c809c06ed01c320cf3d03671500de6bb0e0d6
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-21 17:24:15 +0530

    initial commit

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
-import contact1 from "./contact1.png"
-import "./Contact.css"
+import React, { useState } from "react";
+import contact1 from "./contact1.png";
+import "./Contact.css";
 
 const Contact = () => {
   const [data, setData] = useState({
@@ -8,23 +8,24 @@ const Contact = () => {
     phone: "",
     email: "",
     subject: "",
-    message: "",})
+    message: "",
+  });
 
   const InputEvent = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     setData((preVal) => {
       return {
         ...preVal,
         [name]: value,
-      }
-    })
-  }
+      };
+    });
+  };
 
   // On clicking on Submit button in contect form these sms were display on home screen of users
 
   const formSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     alert(
       `My name is ${data.fullname}. 
 	My phone number is ${data.phone}. 
@@ -32,39 +33,43 @@ const Contact = () => {
 	My Subject on  ${data.subject}. 
 	Here is my message I want to say : ${data.message}. 
 	`
-    )
-  }
+    );
+  };
   return (
     <>
-      <section className='Contact' id='contact'>
-        <div className='container top'>
-          <div className='heading text-center'>
+      <section className="Contact" id="contact">
+        <div className="container top">
+          <div className="heading text-center">
             {/* <h4>CONTACT</h4> */}
             <h1>Contact With Me</h1>
           </div>
 
-          <div className='content d_flex'>
-            <div className='left'>
-              <div className='box box_shodow'>
-                <div className='img'>
-                  <img src={contact1} alt='' />
+          <div className="content d_flex">
+            <div className="left">
+              <div className="box box_shodow">
+                <div className="img">
+                  <img src={contact1} alt="" />
                 </div>
-                <div className='details'>
+                <div className="details">
                   <h1>Priyanhu Ranjan.</h1>
                   <p>An Aspiring Software Engineer</p>
-                  <p>I am available for part time freelance work. Connect with me via email and social networks.</p> <br />
+                  <p>
+                    I am available for part time freelance work. Connect with me
+                    via email and social networks.
+                  </p>{" "}
+                  <br />
                   <p>Phone: +91**********</p>
                   <p>Email: priyanshumth0808@gmail.com</p> <br />
                   <span>FIND WITH ME</span>
-                  <div className='button f_flex'>
-                    <button className='btn_shadow'>
-                      <i className='fab fa-facebook-f'></i>
+                  <div className="button f_flex">
+                    <button className="btn_shadow">
+                      <i className="fab fa-facebook-f"></i>
                     </button>
-                    <button className='btn_shadow'>
-                      <i className='fab fa-instagram'></i>
+                    <button className="btn_shadow">
+                      <i className="fab fa-instagram"></i>
                     </button>
-                    <button className='btn_shadow'>
-                      <i className='fab fa-twitter'></i>
+                    <button className="btn_shadow">
+                      <i className="fab fa-twitter"></i>
                     </button>
                   </div>
                 </div>
@@ -73,32 +78,58 @@ const Contact = () => {
 
             {/* On submiting the contect form these sms were display on home screen of users  */}
 
-            <div className='right box_shodow'>
+            <div className="right box_shodow">
               <form onSubmit={formSubmit}>
-                <div className='f_flex'>
-                  <div className='input row'>
+                <div className="f_flex">
+                  <div className="input row">
                     <span>YOUR NAME</span>
-                    <input type='text' name='fullname' value={data.fullname} onChange={InputEvent} />
+                    <input
+                      type="text"
+                      name="fullname"
+                      value={data.fullname}
+                      onChange={InputEvent}
+                    />
                   </div>
-                  <div className='input row'>
+                  <div className="input row">
                     <span>PHONE NUMBER </span>
-                    <input type='number' name='phone' value={data.phone} onChange={InputEvent} />
+                    <input
+                      type="number"
+                      name="phone"
+                      value={data.phone}
+                      onChange={InputEvent}
+                    />
                   </div>
                 </div>
-                <div className='input'>
+                <div className="input">
                   <span>EMAIL </span>
-                  <input type='email' name='email' value={data.email} onChange={InputEvent} />
+                  <input
+                    type="email"
+                    name="email"
+                    value={data.email}
+                    onChange={InputEvent}
+                  />
                 </div>
-                <div className='input'>
+                <div className="input">
                   <span>SUBJECT </span>
-                  <input type='text' name='subject' value={data.subject} onChange={InputEvent} />
+                  <input
+                    type="text"
+                    name="subject"
+                    value={data.subject}
+                    onChange={InputEvent}
+                  />
                 </div>
-                <div className='input'>
+                <div className="input">
                   <span>YOUR MESSAGE </span>
-                  <textarea cols='30' rows='10' name='message' value={data.message} onChange={InputEvent}></textarea>
+                  <textarea
+                    cols="30"
+                    rows="10"
+                    name="message"
+                    value={data.message}
+                    onChange={InputEvent}
+                  ></textarea>
                 </div>
-                <button className='btn_shadow'>
-                  SEND MESSAGE <i className='fa fa-long-arrow-right'></i>
+                <button className="btn_shadow">
+                  SEND MESSAGE <i className="fa fa-long-arrow-right"></i>
                 </button>
               </form>
             </div>
@@ -106,7 +137,7 @@ const Contact = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
