efd9ed9f952986fbc78b58f6703050137e0eb596
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-24 00:23:51 +0530

    Intial commit

--- src/Components/Hero/Home.js
+++ src/Components/Hero/Home.js
@@ -5,21 +5,22 @@ import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
   const skills = [
-    { id: 1, img: "./assets/asset 5.png" },
-    { id: 2, img: "./assets/asset 6.png" },
-    { id: 3, img: "./assets/asset 7.png" },
-    { id: 4, img: "./assets/asset 8.png" },
-    { id: 5, img: "./assets/asset 9.png" },
-    { id: 6, img: "./assets/asset 5.png" },
-    { id: 7, img: "./assets/asset 11.png" },
-    { id: 8, img: "./assets/asset 12.png" },
-    { id: 9, img: "./assets/asset 13.png" },
-    { id: 10, img: "./assets/asset 14.png" },
-    { id: 10, img: "./assets/asset 15.png" },
-    { id: 10, img: "./assets/asset 16.png" },
-    { id: 10, img: "./assets/asset 17.png" },
-    { id: 10, img: "./assets/asset 18.png" },
-    { id: 10, img: "./assets/asset 19.png" },
+    { id: "html", img: "./assets/asset 5.png" },
+    { id: "css", img: "./assets/asset 6.png" },
+    { id: "js", img: "./assets/asset 7.png" },
+    { id: "React", img: "./assets/asset 3.png" },
+    { id: "material ui", img: "./assets/asset 8.png" },
+    { id: "mongoDb", img: "./assets/asset 9.png" },
+    { id: "jQuery", img: "./assets/asset 10.png" },
+    { id: "java", img: "./assets/asset 11.png" },
+    { id: "C++", img: "./assets/asset 12.png" },
+    { id: "C", img: "./assets/asset 13.png" },
+    { id: "", img: "./assets/asset 14.png" },
+    { id: "Netlify", img: "./assets/asset 15.png" },
+    { id: "Git VCS", img: "./assets/asset 16.png" },
+    { id: "Figma", img: "./assets/asset 17.png" },
+    { id: "Github", img: "./assets/asset 18.png" },
+    { id: "Team Work", img: "./assets/asset 19.png" },
   ];
   return (
     <>
