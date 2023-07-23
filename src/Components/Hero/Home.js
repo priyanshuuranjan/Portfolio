e55048055c9d3131a09a98f6086b9d40ed72a538
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-23 20:06:26 +0530

    Intial commit

--- src/Components/Hero/Home.js
+++ src/Components/Hero/Home.js
@@ -1,38 +1,25 @@
 import React from "react";
 import "./Home.css";
-// import Card from './Card';
-
-
-// import hero from "../pic/hero.png";
-
-
 import hero1 from "../pic/hero1.jpg";
-// import skill1 from "../assets/asset 4.png";
-// import skill2 from "../assets/asset 5.png";
-// import skill3 from "../pic/skill3.png";
-// import skill4 from "../assets/asset 6.png";
-// import skill5 from "../assets/asset 7.png";
-// import skill6 from "../assets/asset 8.png";
-// import skill7 from "../assets/asset 9.png";
-// import skill8 from "../assets/asset 10.png";
-// import skill9 from "../assets/asset 11.png";
-// import skill10 from "../assets/asset 12.png";
-// import skill11 from "../assets/asset 13.png";
-
 import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
   const skills = [
-    { id: 1, img: "./assets/asset 5.png"}
-    // { id: 2, img: "./assets/asset 6.png" },
-    // { id: 3, img: "./assets/asset 6.png" },
-    // { id: 4, img:"./assets/asset 6.png" },
-    // { id: 5, img: "./assets/asset 6.png" },
-    // { id: 6, img: "./assets/asset 6.png" },
-    // { id: 7, img: "./assets/asset 6.png" },
-    // { id: 8, img: "./assets/asset 6.png" },
-    // { id: 9, img: "./assets/asset 6.png"},
-    // { id: 10, img: "./assets/asset 6.png" }
+    { id: 1, img: "./assets/asset 5.png" },
+    { id: 2, img: "./assets/asset 6.png" },
+    { id: 3, img: "./assets/asset 7.png" },
+    { id: 4, img: "./assets/asset 8.png" },
+    { id: 5, img: "./assets/asset 9.png" },
+    { id: 6, img: "./assets/asset 10.png" },
+    { id: 7, img: "./assets/asset 11.png" },
+    { id: 8, img: "./assets/asset 12.png" },
+    { id: 9, img: "./assets/asset 13.png" },
+    { id: 10, img: "./assets/asset 14.png" },
+    { id: 10, img: "./assets/asset 15.png" },
+    { id: 10, img: "./assets/asset 16.png" },
+    { id: 10, img: "./assets/asset 17.png" },
+    { id: 10, img: "./assets/asset 18.png" },
+    { id: 10, img: "./assets/asset 19.png" },
   ];
   return (
     <>
@@ -47,7 +34,11 @@ const Home = () => {
               a
               <span>
                 <Typewriter
-                  words={[" Professional Coder.", " Web Developer.", " Android Developer."]}
+                  words={[
+                    " Professional Coder.",
+                    " Web Developer.",
+                    " Android Developer.",
+                  ]}
                   loop
                   cursor
                   cursorStyle="|"
@@ -83,45 +74,16 @@ const Home = () => {
                 </div>
               </div>
               <div className="col_1">
-      <h4>BEST SKILL ON</h4>
-      <div className="btn_shadow">
-      {skills.map((skill) => (
-                <img src={skill.img} alt={`skill-${skill.id}`} key={skill.id} />
-              ))}
-      </div>
-                {/* <button className="btn_shadow">
-                  <img src={skill2} alt="" />
-                </button>
+                <h4>BEST SKILL ON</h4>
                 <button className="btn_shadow">
-                  <img src={skill3} alt="" />
+                  {skills.map((skill) => (
+                    <img
+                      src={skill.img}
+                      alt={`skill-${skill.id}`}
+                      key={skill.id}
+                    />
+                  ))}
                 </button>
-                <button className="btn_shadow">
-                  <img src={skill4} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill5} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill6} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill7} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill8} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill9} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill10} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill11} alt="" />
-                </button>
-                <button className="btn_shadow">
-                  <img src={skill4} alt="" />
-                </button> */}
               </div>
             </div>
           </div>
@@ -137,6 +99,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
-
