1bca09c9bc5cc59fb7faf4d661ac94a32973798c
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-25 00:51:44 +0530

    Intial commit

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,29 +1,21 @@
-import React from "react";
-import "./Portfolio.css";
-import Card from "./Card";
-import Portfolio_data from "./Portfolio_data";
+import React from "react"
+import "./Portfolio.css"
+import Card from "./Card"
+import Portfolio_data from "./Portfolio_data"
 
 const Portfolio = () => {
   return (
     <>
-      <section className="Portfolio top" id="portfolio">
-        <div className="container">
-          <div className="heading text-center ">
+      <section className='Portfolio top' id='portfolio'>
+        <div className='container'>
+          <div className='heading text-center '>
             <h4>Projects And Certificates</h4>
             <h1>My Projects</h1>
           </div>
 
-          <div className="content grid">
+          <div className='content grid'>
             {Portfolio_data.map((value, index) => {
-              return (
-                <Card
-                  key={index}
-                  image={value.image}
-                  category={value.category}
-                  des={value.des}
-                  title={value.title}
-                />
-              );
+              return <Card key={index} image={value.image} category={value.category} des={value.des} title={value.title} />
             })}
 
             {/*<div className='box btn_shadow '>
@@ -43,18 +35,11 @@ const Portfolio = () => {
                 </a>
               </div>
             </div>*/}
-            <div className="container">
-          <div className="heading text-center ">
-            {/* <h4>Projects And Certificates</h4> */}
-            <h1> Certificates</h1>
-          </div>
-          </div>
-            
           </div>
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Portfolio;
+export default Portfolio
