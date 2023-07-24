4c94fdec8a60b70491e18c2f8ccfcef3d04319ba
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-25 00:42:47 +0530

    Intial commit

--- src/Components/Portfolio/Portfolio_data.js
+++ src/Components/Portfolio/Portfolio_data.js
@@ -14,7 +14,7 @@ const Portfolio_data = [
     // totalLike: "750",
     title: "Mobile app landing design & maintain",
     image: "./assets/asset 19.png", // this is the path for adding ans sowing the images in projects i have to put the images in publick folder.
-    des:"made adhar applications ",
+    des: "Discover our comprehensive physiotherapy website, built using React JS, with multiple pages offering a wide range of treatment options. Experience the convenience of video consultations and online booking. Emphasizing natural therapies, we also welcome email inquiries, ensuring seamless communication for optimal care and personalized service. ",
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ const Portfolio_data = [
     // totalLike: "630",
     title: "Newborn baby photo frames",
     image: "./assets/asset 19.png",
-    des:"Explore our website featuring a delightful array of newborn baby photo frames, available in various styles and designs. Users can effortlessly select multiple frames and place orders, all with the added bonus of being completely free for everyone.",
+    des: "Explore our website featuring a delightful array of newborn baby photo frames, available in various styles and designs. Users can effortlessly select multiple frames and place orders, all with the added bonus of being completely free for everyone.",
   },
   {
     id: 4,
@@ -30,7 +30,7 @@ const Portfolio_data = [
     // totalLike: "360",
     title: "Mobile app landing design & Services",
     image: "./assets/asset 19.png",
-    des:" i desighn many tempaltes on figma",
+    des: " i desighn many tempaltes on figma",
   },
   {
     id: 5,
@@ -38,15 +38,16 @@ const Portfolio_data = [
     // totalLike: "280",
     title: "Design and made best mail sender website",
     image: "./assets/asset 19.png",
-    des:"mail sender ",
+    des: "mail sender ",
   },
   {
     id: 6,
     category: "Physiotherapy Website",
     // totalLike: "690",
     title: "App for tecnology & services",
-    image: "https://rainbowit.net/html/inbio/assets/images/portfolio/portfolio-01.jpg",
-    des:"cbjdzbckasbcv",
+    image:
+      "https://rainbowit.net/html/inbio/assets/images/portfolio/portfolio-01.jpg",
+    des: "cbjdzbckasbcv",
   },
 ];
 export default Portfolio_data;
