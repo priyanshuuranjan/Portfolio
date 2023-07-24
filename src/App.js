6d154f27af0b164477be0c95f1a376320ffe5a01
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-25 01:27:45 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Header from "./Components/Head/Header";
 import Home from "./Components/Hero/Home";
-import Features from "./Components/Features/Features";
+// import Features from "./Components/Features/Features";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Resume from "./Components/Resume/Resume";
-import Testimonial from "./Components/Testimonial/Testimonial";
+// import Testimonial from "./Components/Testimonial/Testimonial";
 import Contact from "./Components/Contact/Contact";
 // import Footer from "./Components/Footer/Footer";
 import "./App.css";
@@ -16,10 +16,10 @@ function App() {
      {/* Routing Part Done Here */}
       <Header />
       <Home />
-      <Features />
+      {/* <Features /> */}
       <Portfolio />
       <Resume />
-      <Testimonial />
+      {/* <Testimonial /> */}
       <Contact />
       {/* <Footer /> */}
     </>
