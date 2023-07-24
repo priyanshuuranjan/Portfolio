80d83b17828ed4225c227f338d8253754ea2143e
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-25 01:16:53 +0530

    Intial commit

--- src/Components/Portfolio/Card.js
+++ src/Components/Portfolio/Card.js
@@ -44,8 +44,7 @@ const Card = (props) => {
               <span>Featured - Design</span>
               <h1>{props.title}</h1>
               <h2>{props.des}</h2>
-              {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate distinctio assumenda explicabo veniam temporibus eligendi.</p>
-              <p>Consectetur adipisicing elit. Cupiditate distinctio assumenda. dolorum alias suscipit rerum maiores aliquam earum odit, nihil culpa quas iusto hic minus!</p> */}
+
               <div className="button f_flex mtop">
                 <button className="btn_shadow">
                   VIEW PROJECT<i class="fas fa-chevron-right"></i>
