1ee74e9abc1564baeaba35b1b53d4a3437105e20
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-25 01:26:32 +0530

    Intial commit

--- src/Components/Portfolio/Card.js
+++ src/Components/Portfolio/Card.js
@@ -20,9 +20,7 @@ const Card = (props) => {
         </div>
         <div className="category d_flex">
           <span onClick={toggleModal}>{props.category}</span>
-          {/* <label>
-            <i className='far fa-heart'></i> {props.totalLike}
-          </label> */}
+          
         </div>
         <div className="title">
           <h2 onClick={toggleModal}>{props.title}</h2>
@@ -62,69 +60,3 @@ const Card = (props) => {
 };
 
 export default Card;
-
-/* this code is used for increase the likes when user  click on like button  */
-// import React, { useState } from "react"
-
-// const Card = (props) => {
-//   const [modal, setModal] = useState(false)
-//   const [likes, setLikes] = useState(props.totalLike)
-
-//   const toggleModal = () => {
-//     setModal(!modal)
-//   }
-
-//   const handleLikeClick = () => {
-//     setLikes(likes + 1)
-//   }
-
-//   if (modal) {
-//     document.body.classList.add("active-modal")
-//   } else {
-//     document.body.classList.remove("active-modal")
-//   }
-//   return (
-//     <>
-//       <div className='box btn_shadow '>
-//         <div className='img'>
-//           <img src={props.image} alt='' onClick={toggleModal} />
-//         </div>
-//         <div className='category d_flex'>
-//           <span onClick={toggleModal}>{props.category}</span>
-//           <label>
-//             <i className='far fa-heart'></i> {likes}
-//           </label>
-//         </div>
-//         <div className='title'>
-//           <h2 onClick={toggleModal}>{props.title}</h2>
-//           <a href='#popup' className='arrow' onClick={toggleModal}>
-//             <i class='fas fa-arrow-right'></i>
-//           </a>
-//         </div>
-//       </div>
-
-//       {/* Popup box */}
-//       {modal && (
-//         <div className='modal'>
-//           <div onClick={toggleModal} className='overlay'></div>
-//           <div className='modal-content d_flex'>
-//             <div className='modal-img left'>
-//               <img src={props.image} alt='' />
-//             </div>
-//             <div className='modal-text right'>
-//               <span>Featured - Design</span>
-//               <h1>{props.title}</h1>
-//               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate distinctio assumenda explicabo veniam temporibus eligendi.</p>
-//               <p>Consectetur adipisicing elit. Cupiditate distinctio assumenda. dolorum alias suscipit rerum maiores aliquam earum odit, nihil culpa quas iusto hic minus!</p>
-//               <div className='button f_flex mtop'>
-//                 <button className='btn_shadow' onClick={handleLikeClick}>
-//                   LIKE THIS <i class='far fa-thumbs-up'></i>
-//                 </button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   )
-// }
