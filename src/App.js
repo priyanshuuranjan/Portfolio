4f37ca982fdf47059ce999086e6ea1438ec61ac9
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-01 00:28:18 +0530

    End of the project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
 import Header from "./Components/Head/Header";
 import Home from "./Components/Hero/Home";
-// import Features from "./Components/Features/Features";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Resume from "./Components/Resume/Resume";
-// import Testimonial from "./Components/Testimonial/Testimonial";
 import Contact from "./Components/Contact/Contact";
-// import Footer from "./Components/Footer/Footer";
 import "./App.css";
-
 
 function App() {
   return (
     <>
-     {/* Routing Part Done Here */}
+      {/* Routing Part Done Here */}
       <Header />
       <Home />
-      {/* <Features /> */}
       <Portfolio />
       <Resume />
-      {/* <Testimonial /> */}
       <Contact />
-      {/* <Footer /> */}
     </>
   );
 }
