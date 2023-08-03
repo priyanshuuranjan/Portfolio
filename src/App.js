468473e7596fa673b8a7a970785a8db1db558b6d
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-03 21:03:30 +0530

    Adding spinner

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [loadings, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
-    setTimeout(() => setLoading(false), 4000);
+    setTimeout(() => setLoading(false), 3000);
   }, []);
   return (
     <>
