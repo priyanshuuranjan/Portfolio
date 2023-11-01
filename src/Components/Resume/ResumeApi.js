4561e30e7672932a46ec6f2c3bedf82d489ca979
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-11-02 01:54:31 +0530

    All done change photo

--- src/Components/Resume/ResumeApi.js
+++ src/Components/Resume/ResumeApi.js
@@ -5,7 +5,7 @@ const ResumeApi = [
     year: "Lovely Professional University | Phagwara, IN (2020 - Present)",
     title: "B.Tech. in Computer Science",
     desc: "Study Computer Science at LPU College, admitted in 2020. Explored cutting-edge tech, coding, and problem-solving in a dynamic learning environment, preparing for a promising future in the digital world..",
-    // rate: "4.30/5",
+ 
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const ResumeApi = [
     year: "MS Memorial Public School | Bihar, IN  (2018 - 2020) ",
     title: "Intermediate | Science",
     desc: "Study science at MS Memorial Public School for an enriching Intermediate experience. Embrace innovative learning, expert faculty, and a nurturing environment to excel in your academic pursuits and future endeavors.",
-    // rate: "4.50/5",
+    
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const ResumeApi = [
     year: " MS Memorial Public School |Bihar, IN (2005 - 20017) ",
     title: "Higher School",
     desc: "Higher School at MS Memorial Public School offers comprehensive education, fostering academic excellence, personal growth, and character development in a nurturing environment, preparing students for future success.",
-    // rate: "4.80/5 ",
+   
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const ResumeApi = [
     year: "Domain: Web Development | Skills honed: HTML, CSS, React, Bootstrap and Firebase.",
     title: "Complaint Management System",
     desc: "The single-page website enables users to choose and describe hostel-life problems, submitting them for resolution. Data is stored on Firebase, and our management efficiently addresses and resolves these issues.",
-    // rate: "4.70/5 ",
+  
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const ResumeApi = [
     year: " Domain: Web Development | Skills honed: HTML, CSS, JavaScript and Bootstrap.  ",
     title: "Photography Website ",
     desc: "Explore our website featuring a delightful array of newborn baby photo frames, available in various styles and designs. Users can effortlessly select multiple frames and place orders, all with the added bonus of being completely free for everyone.",
-    // rate: "4.95/5 ",
+  
   },
   {
     id: 6,
